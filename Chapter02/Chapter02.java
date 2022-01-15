cf38d8ad8faeddac1592788aae331c08111a6d46
hiroko-i <90242962+hiroko-i@users.noreply.github.com>
2022-01-15 22:00:49 +0900

    java 変数

--- Chapter02/Chapter02.java
+++ Chapter02/Chapter02.java
@@ -4,5 +4,8 @@ public class Chapter02 {
 	public static void main(String[] args) {
 		System.out.println("Hello WEBCAMP!");
 		System.out.println("Hello Java!");
+		int x;
+		x = 6;
+		System.out.println(x * x * 3.14);
 	}
 }
