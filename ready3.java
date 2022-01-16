57b9cab8ab0e9df6140f1e3da4dde69df6189383
hiroko-i <90242962+hiroko-i@users.noreply.github.com>
2022-01-16 21:36:20 +0900

    エスケープシーケンス

---
+++ ready3.java
@@ -0,0 +1,9 @@
+
+public class ready3 {
+	public static void main(String[] args) {
+		System.out.println("二重引用符は\"です");
+		System.out.println("引用符は\'です");
+		System.out.println("円マークは¥1400です");
+		System.out.println("改行は\n¥1400です");
+	}
+}
