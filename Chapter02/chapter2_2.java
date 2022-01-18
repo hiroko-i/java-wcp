90342c8943d4e15528358b87933b160767d89e2e
hiroko-i <90242962+hiroko-i@users.noreply.github.com>
2022-01-18 22:20:48 +0900

    型自動変換・強制変換

--- Chapter02/chapter2_2.java
+++ Chapter02/chapter2_2.java
@@ -6,18 +6,18 @@ public class chapter2_2 {
 		a = 100;
 		a++;
 		System.out.println(a);
-		
+
 		int b;
 		b = 100;
 		b += 1;
 		System.out.println(b);
-		
+
 		int c;
 		c = 100;
 		c = c + 1;
 		System.out.println(c);
 //		上記３つ同様　減算”−−”も
-		
+
 //		基本的にオペランドの前後どちらにつけてもOK。しかし他の演算子と使うときの違い
 		int d = 10;
 		int e = 10;
@@ -25,5 +25,16 @@ public class chapter2_2 {
 //		①変数dが１増え　②10加算
 		System.out.println(e++ + 10);
 //		①変数eに10足した値が表示　②変数eに1加算
+
+		float f = 3;
+		double g = f;
+		System.out.println(f);
+//		int3 →　float3.0F に自動変換
+		System.out.println(g);
+//		float型がdouble型に自動変換
+
+		int age = (int)3.2;
+		System.out.println(age);
+//		強制型変換　キャスト演算子
 	}
 }
