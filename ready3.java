f9d08b20c9d21eb049be287a90e597ad1d3487a9
hiroko-i <90242962+hiroko-i@users.noreply.github.com>
2022-01-20 22:54:55 +0900

    未コミット分一括コミット

--- ready3.java
+++ ready3.java
@@ -4,6 +4,6 @@ public class ready3 {
 		System.out.println("二重引用符は\"です");
 		System.out.println("引用符は\'です");
 		System.out.println("円マークは¥1400です");
-		System.out.println("改行は\n¥1400です");
+		System.out.println("改行は\nです");
 	}
 }
