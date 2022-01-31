2f46616cb967685945bbd0e11db641e8a60372b0
hiroko-i <90242962+hiroko-i@users.noreply.github.com>
2022-01-31 20:57:44 +0900

    メソッドの呼び出し

---
+++ chapter5/chapter5_1.java
@@ -0,0 +1,12 @@
+package chapter5;
+
+public class chapter5_1 {
+	public static void main(String[] args) {
+		System.out.println("メソッドを呼び出します");
+		hello();
+		System.out.println("メソッドの呼び出し終了");
+	}
+	public static void hello() {		
+		System.out.println("森さんこんにちは");
+	}
+}
