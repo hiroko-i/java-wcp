3db901c32b69a3f78af2e89db6f2b06c2741ed54
hiroko-i <90242962+hiroko-i@users.noreply.github.com>
2022-02-02 22:07:53 +0900

    メソッド呼び出し

--- chapter5/chapter5_1.java
+++ chapter5/chapter5_1.java
@@ -1,12 +1,22 @@
 package chapter5;
 
 public class chapter5_1 {
+	public static void methodA() {
+		System.out.println("methodA");
+		methodB();
+	}
+	public static void methodB() {
+		System.out.println("methodB");
+	}
+
+//	プログラムは必ずmainから動き始める
 	public static void main(String[] args) {
 		System.out.println("メソッドを呼び出します");
 		hello();
+		methodA();
 		System.out.println("メソッドの呼び出し終了");
 	}
-	public static void hello() {		
+	public static void hello() {
 		System.out.println("森さんこんにちは");
 	}
 }
